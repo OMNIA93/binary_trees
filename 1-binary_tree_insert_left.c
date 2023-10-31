3c5c16ce0c72376b6c45a4f5334dafd0f2992ca4
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-11-01 01:24:13 +0200

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,10 +9,10 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 		return (NULL);
@@ -23,10 +23,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	if (parent->left != NULL)
+	if (parent->left)
 	{
-	new_node->left = parent->left;
-	new_node->left->parent = new_node;
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
 	}
 
 	parent->left = new_node;
