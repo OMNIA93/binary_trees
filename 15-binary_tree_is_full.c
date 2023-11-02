bf0956a42fbd4c214cdb84e5c5d0dee6c89948fb
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-11-03 01:25:06 +0200

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -15,7 +15,7 @@ return (0);
 if (tree->left == NULL && tree->right == NULL)
 return (1);
 
-if ((tree->left == NULL && tree->right == NULL) || (tree->left && tree->right))
+if ((tree->left == NULL && tree->right == NULL) || (tree->left == NULL && tree->right == NULL))
 return (0);
 
 return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
