95e195af09200d0a0bdc12913547fcb279ec08ec
OMNIA93 <omniannnnnnn@gmail.com>
2023-11-02 16:49:55 -0700

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,36 +2,31 @@
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
- *
- * @tree: Pointer to the root node of the tree to measure the balance factor
- *
- * Return: The balance factor of the tree
+ * @tree: Pointer to the root node of the tree to measure
+ * Return: Balance factor of the binary tree, or 0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
 
-int (left_height = binary_tree_height(tree->left);
-int (right_height = binary_tree_height(tree->right);
-
-return (left_height - right_height);
+return (compute_tree_height(tree->left) - compute_tree_height(tree->right));
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- *
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: The height of the tree
+ * compute_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ * Return: Height of the binary tree, or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int compute_tree_height(const binary_tree_t *tree)
 {
+int left_height, right_height;
+
 if (tree == NULL)
-return (0);
+return (-1);
 
-int (left_height = binary_tree_height(tree->left);
-int (right_height = binary_tree_height(tree->right);
+left_height = tree->left ? 1 + compute_tree_height(tree->left) : 0;
+right_height = tree->right ? 1 + compute_tree_height(tree->right) : 0;
 
-return (1 + (left_height > right_height ? left_height : right_height));
+return (left_height > right_height ? left_height : right_height);
 }
