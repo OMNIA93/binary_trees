72366800f6a7c2c4665689b0e5dceaaf8c9f4255
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-11-02 17:21:22 +0200

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,18 +8,15 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+binary_tree_t *grandparent;
+
 if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 return (NULL);
 
+grandparent = node->parent->parent;
 
-binary_tree_t *parent = node->parent;
-binary_tree_t *grandparent = parent->parent;
-
-if (grandparent->left == parent)
+if (grandparent->left == node->parent)
 return (grandparent->right);
-}
-else
-{
+
 return (grandparent->left);
-}
 }
