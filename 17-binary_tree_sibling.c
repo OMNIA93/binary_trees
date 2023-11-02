2c912db93048602551b06347ade3383fe8c8597b
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-11-02 17:57:33 +0200

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,8 +11,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 if (node == NULL || node->parent == NULL)
 return (NULL);
 
-if (parent->right == node)
-return (parent->left);
+	if (node->parent->right == node)
+		return (node->parent->left);
 
-return (node->parent->right);
+	return (node->parent->right);
 }
