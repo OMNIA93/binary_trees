549200b5e2a09c00586a386f79fb5f8af899a81d
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-11-03 00:44:12 +0200

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,20 +7,23 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-size_t right_height, left_height;
-if (tree == NULL)
-return (0);
+	size_t left_height, right_height;
 
-right_height = binary_tree_height(tree->right);
-left_height = binary_tree_height(tree->left);
+	if (tree == NULL)
+		return (0);
 
-if (!tree->right && !tree->left)
-return (1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-return (binary_tree_is_perfect(tree->right) &&
-				binary_tree_is_perfect(tree->left));
+	if (left_height != right_height)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
 }
-
 
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -29,13 +32,13 @@ return (binary_tree_is_perfect(tree->right) &&
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t right_height, left_height;
+	size_t left_height, right_height;
 
-if (tree == NULL)
-return (0);
+	if (tree == NULL)
+		return (0);
 
-right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-leftt_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-return (right_height > left_height ? right_height : left_height);
+	return (left_height > right_height ? left_height : right_height);
 }
